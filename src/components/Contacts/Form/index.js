e7b44fe5db14ac4b182f5cca9a0b5dea6fbe4e53
ezgikarali <ezgi.karali505@gmail.com>
2022-09-19 20:27:14 +0300

    first

---
+++ src/components/Contacts/Form/index.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+const initialFormValues = { fullName: "", phoneNumber: "" };
+
+function Form({ addContact, contacts }) {
+  const [form, setForm] = useState(initialFormValues);
+
+  useEffect(() => {
+    setForm(initialFormValues);
+  }, [contacts]);
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (form.fullName === "" || form.phoneNumber === "") {
+      return false;
+    }
+    addContact([...contacts, form]);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          name="fullName"
+          value={form.fullName}
+          placeholder="Full Name"
+          onChange={onChangeInput}
+        />
+      </div>
+      <div>
+        {" "}
+        <input
+          name="phoneNumber"
+          value={form.phoneNumber}
+          placeholder="Phone Number"
+          onChange={onChangeInput}
+        />
+      </div>
+      <div className="btn">
+        {" "}
+        <button>Add</button>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
---
+++ src/components/Contacts/index.js
@@ -0,0 +1,38 @@
+import {useState, useEffect} from 'react'
+
+import "./style.css"
+import List from "./List"
+import Form from "./Form"
+function Contacts() {
+  const [contacts, setContacts] = useState([
+  {
+fullName: "Ezgi",
+phoneNumber: "388342934",
+  },
+{
+fullName: "Miray",
+phoneNumber: "88773940",
+  },
+  {
+fullName: "Dafi",
+phoneNumber: "390973094",
+  }
+  ]);
+  
+  
+  
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
+  
+  
+  return (
+    <div id='container'>
+      <h1>Contacts</h1>
+        <List contacts={contacts}/>
+        <Form addContact={setContacts} contacts={contacts}/>
+    </div>
+  )
+}
+
+export default Contacts;
